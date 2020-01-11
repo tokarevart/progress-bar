eb247de3e5d5b25137fceb4543d62e6820f92580
Tokarev Artem <tokarev28.art@gmail.com>
2020-01-11 11:34:31 +0500

    Добавьте файлы проекта.

---
+++ main.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <chrono>
+#include <thread>
+#include "progress-bar.h"
+
+int main() {
+    const std::size_t max = 1000;
+
+    progress_bar bar(max, 70);
+    for (std::size_t i = 0; i < max; ++i) {
+        ++bar;
+        using namespace std::chrono_literals;
+        std::this_thread::sleep_for(1ms);
+    }
+
+    return 0;
+}
---
+++ progress-bar.h
@@ -0,0 +1,81 @@
+#pragma once
+#include <iostream>
+#include <string>
+
+class progress_bar {
+public:
+    void restart() {
+        m_count = 0;
+        display();
+    }
+    void stop() {
+        display();
+        std::cout << std::endl;
+        m_is_started = false;
+    }
+
+    std::size_t bar_width() const {
+        return m_bar_width;
+    }
+    std::size_t max() const {
+        return m_max;
+    }
+    std::size_t count() const {
+        return m_count;
+    }
+    // return new count value
+    std::size_t set_count(std::size_t new_value) {
+        if (new_value < max()) {
+            m_count = new_value;
+            display();
+        } else {
+            m_count = max();
+            stop();
+        }
+        return m_count;
+    }
+
+    std::size_t operator+=(std::size_t increment) {
+        return set_count(count() + increment);
+    }
+    std::size_t operator-=(std::size_t decrement) {
+        return set_count(count() - decrement);
+    }
+    std::size_t operator++() {
+        return set_count(count() + 1);
+    }
+    std::size_t operator--() {
+        return set_count(count() - 1);
+    }
+
+    progress_bar(std::size_t max, std::size_t width, char complete = '=', char incomplete = ' ') :
+        m_max(max), m_bar_width(width), m_complete_char(complete), m_incomplete_char(incomplete) {}
+
+
+private:
+    bool m_is_started = true;
+    std::size_t m_count = 0;
+
+    const std::size_t m_max;
+    const std::size_t m_bar_width;
+    const char m_complete_char;
+    const char m_incomplete_char;
+
+    void display() const {
+        if (!m_is_started)
+            return;
+
+        auto completed_width = (bar_width() * count()) / max();
+
+        std::string bar = "\r[";
+        bar.reserve(bar_width() + 10);
+        for (std::size_t i = 0; i < completed_width; ++i)
+            bar += m_complete_char;
+        for (std::size_t i = completed_width; i < bar_width(); ++i)
+            bar += m_incomplete_char;
+        bar += "] " + std::to_string((count() * 100) / max()) + "%";
+
+        std::cout << bar;
+        std::cout.flush();
+    }
+};
